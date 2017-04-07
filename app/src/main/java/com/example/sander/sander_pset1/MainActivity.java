a9592313fb6d7acf80dc21b3e615c24d1d6e3bb5
grelon <642c06ae@opayq.com>
2017-04-07 16:46:33 +0200

    Done: - Layout portrait - show functions - buttons
    ToDo:
    - Implement landscape orientation
    - (optional) Create multi purpose show function

--- app/src/main/java/com/example/sander/sander_pset1/MainActivity.java
+++ app/src/main/java/com/example/sander/sander_pset1/MainActivity.java
@@ -42,19 +42,132 @@ public class MainActivity extends AppCompatActivity {
         shoes = (ImageView) findViewById(R.id.img_shoes);
     }
 
-    public void toggleShow(View view) {
+    public void showArms(View view) {
         // if visibility is VISIBLE
-        Log.d("Visible", "Hiephoi1");
+        if (arms.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            arms.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            arms.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showBody(View view) {
+        // if visibility is VISIBLE
+        if (body.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            body.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            body.setVisibility(View.VISIBLE);
+        }
+    }
+    public void showEars(View view) {
+        // if visibility is VISIBLE
         if (ears.getVisibility() == View.VISIBLE) {
             // set visbility to INVISIBLE
-            Log.d("Visible", "Hiephoi2");
             ears.setVisibility(View.INVISIBLE);
-            Log.d("Visible", "Hiephoi3");
         }
-
         else {
             // set visibility to VISIBLE
             ears.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showEyebrows(View view) {
+        // if visibility is VISIBLE
+        if (eyebrows.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            eyebrows.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            eyebrows.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showEyes(View view) {
+        // if visibility is VISIBLE
+        if (eyes.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            eyes.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            eyes.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showGlasses(View view) {
+        // if visibility is VISIBLE
+        if (glasses.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            glasses.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            glasses.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showHat(View view) {
+        // if visibility is VISIBLE
+        if (hat.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            hat.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            hat.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showMouth(View view) {
+        // if visibility is VISIBLE
+        if (mouth.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            mouth.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            mouth.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void showMustache(View view) {
+        // if visibility is VISIBLE
+        if (mustache.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            mustache.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            mustache.setVisibility(View.VISIBLE);
+        }
+    }
+    public void showNose(View view) {
+        // if visibility is VISIBLE
+        if (nose.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            nose.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            nose.setVisibility(View.VISIBLE);
+        }
+    }
+    public void showShoes(View view) {
+        // if visibility is VISIBLE
+        if (nose.getVisibility() == View.VISIBLE) {
+            // set visbility to INVISIBLE
+            nose.setVisibility(View.INVISIBLE);
+        }
+        else {
+            // set visibility to VISIBLE
+            nose.setVisibility(View.VISIBLE);
         }
     }
 }
